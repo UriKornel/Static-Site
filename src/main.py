e2c2a5d9ba119ed77ca067f4b859d4471e080c84
Uri Kornel <uri.kornel@protonmail.com>
2026-04-26 17:26:39 +0200

    asd

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
-from split_nodes_delimiter import split_nodes_delimiter
+# from textnode import TextNode, TextType
+# from htmlnode import HTMLNode
+# from test_split_nodes_delimeter import split_nodes_delimiter
+from cpy import cpy
 
 
 def main():
@@ -11,20 +12,24 @@ def main():
     # html = HTMLNode(None, None, None, prop)
     # print(html.props_to_html())
 
-    nodes = [
-        TextNode("This is a node `code`", TextType.TEXT),
-        TextNode("This is the second node `code`", TextType.TEXT)
-    ]
-    new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-    answer = [
-        TextNode("This is a node ", TextType.TEXT),
-        TextNode("code", TextType.CODE),
-        TextNode("", TextType.TEXT),
-        TextNode("This is the second node ", TextType.TEXT),
-        TextNode("code", TextType.CODE)
-    ]
-    print(f"input: {nodes}")
-    print(f"output: {new_nodes}")
-    print(f"answer: {answer}")
+    # nodes = [
+    #     TextNode("This is a node `code`", TextType.TEXT),
+    #     TextNode("This is the second node `code`", TextType.TEXT)
+    # ]
+    # new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    # answer = [
+    #     TextNode("This is a node ", TextType.TEXT),
+    #     TextNode("code", TextType.CODE),
+    #     TextNode("", TextType.TEXT),
+    #     TextNode("This is the second node ", TextType.TEXT),
+    #     TextNode("code", TextType.CODE)
+    # ]
+    # print(f"input: {nodes}")
+    # print(f"output: {new_nodes}")
+    # print(f"answer: {answer}")
+
+    src = "static/"
+    dest = "public/"
+    cpy(src, dest)
 
 main()
