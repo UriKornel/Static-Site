32de70c3d7213ef55a626e4cd80813464b29b52f
Uri Kornel <uri.kornel@protonmail.com>
2026-04-29 16:47:37 +0200

    generate multiple pages from multiple files

---
+++ src/generate_pages_recursive.py
@@ -0,0 +1,26 @@
+import os
+from generate_page import generate_page
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_path_content = os.path.abspath(dir_path_content)
+    template_path = os.path.abspath(template_path)
+    dest_dir_path = os.path.abspath(dest_dir_path)
+
+    print(f"content path: {dir_path_content}")
+    print(f"template path: {template_path}")
+    print(f"destination path: {dest_dir_path}")
+
+    if not os.path.exists(dir_path_content):
+        raise Exception("Generate error: Content Path does not exist")
+    elif not os.path.exists(template_path):
+        raise Exception("Generate error: template does not exist")
+
+    for file in os.listdir(dir_path_content):
+        src_path = os.path.join(dir_path_content, file)
+        if os.path.isfile(src_path) and src_path.endswith(".md"):
+            generate_page(src_path, template_path, dest_dir_path)
+        elif os.path.isdir(src_path):
+            dest_path = os.path.join(dest_dir_path, file)
+            generate_pages_recursive(src_path, template_path, dest_path)
+
+
--- src/main.py
+++ src/main.py
@@ -2,40 +2,17 @@
 # from htmlnode import HTMLNode
 # from test_split_nodes_delimeter import split_nodes_delimiter
 from cpy import cpy
-from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 
 
 def main():
-    # prop = {
-    #     "href": "https://www.google.com",
-    #     "target": "_blank"
-    # }
-    # html = HTMLNode(None, None, None, prop)
-    # print(html.props_to_html())
-
-    # nodes = [
-    #     TextNode("This is a node `code`", TextType.TEXT),
-    #     TextNode("This is the second node `code`", TextType.TEXT)
-    # ]
-    # new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-    # answer = [
-    #     TextNode("This is a node ", TextType.TEXT),
-    #     TextNode("code", TextType.CODE),
-    #     TextNode("", TextType.TEXT),
-    #     TextNode("This is the second node ", TextType.TEXT),
-    #     TextNode("code", TextType.CODE)
-    # ]
-    # print(f"input: {nodes}")
-    # print(f"output: {new_nodes}")
-    # print(f"answer: {answer}")
-
     src = "static/"
     dest = "public/"
     cpy(src, dest)
 
-    from_path = "content/index.md"
+    from_path = "content"
     template_path = "template.html"
-    dest_path = "public/index.html"
-    generate_page(from_path, template_path, dest_path)
+    dest_path = "public"
+    generate_pages_recursive(from_path, template_path, dest_path)
 
 main()
